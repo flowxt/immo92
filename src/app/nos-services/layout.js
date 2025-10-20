9105e4cda8d1856459bc7ab0b269edd58970ec7e
florian <florianbarjon@icloud.com>
2025-10-20 09:45:23 +0200

    section exemple

---
+++ src/app/nos-services/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Nos services | eXp France - Mont-Valérien",
+  description:
+    "Des solutions personnalisées pour valoriser votre bien immobilier : estimation, rénovation énergétique, division de terrain, mise en valeur.",
+};
+
+export default function NosServicesLayout({ children }) {
+  return children;
+}
